17487818347de03ebca0463dd8d2497e1f14c7e4
kelvinokumu <oumakelvinokumu@gmail.com>
2022-11-11 22:35:25 +0300

    working on the search bar

--- app/src/main/java/com/example/countries_details/MainActivity.kt
+++ app/src/main/java/com/example/countries_details/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.countries_details
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
@@ -13,11 +15,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.countries_details.ui.theme.Countries_detailsTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,10 +39,49 @@ class MainActivity : ComponentActivity() {
 //                ) {
 //                    CountryItem()
 //                }
-                CountryItem()
-
+                Column() {
+                    SearchListItem()
+                    CountryItem()
+                }
             }
         }
+    }
+}
+
+@Composable
+fun SearchListItem() {
+    Card(
+        modifier = Modifier
+            .padding(top = 16.dp)
+            .fillMaxWidth(),
+        elevation = 4.dp,
+        border = BorderStroke(width = 1.2.dp, Color.Blue)
+    ) {
+        Row(content = {
+//            Icon(
+//                Icons.Default.Contacts,
+//                "",
+//                tint = Color.Blue,
+//                modifier = Modifier
+//                    .padding(start = 16.dp, top = 16.dp)
+//            )
+            CountryIcon(
+                Icons.Filled.Place,
+                Modifier.weight(0.15f)
+            )
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(
+                    text = "${"searchData.name"}",
+                    style = TextStyle(
+                        color = Color.Blue,
+                        fontSize = 21.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+                Text(text = "${"searchData.emailId"}", modifier = Modifier.padding(top = 8.dp))
+            }
+        })
+
     }
 }
 
