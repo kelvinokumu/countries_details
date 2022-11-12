09fa9a00edf09093a217df34c04941efaf86cc75
kelvinokumu <oumakelvinokumu@gmail.com>
2022-11-12 22:07:28 +0300

    adding data classes

---
+++ app/src/main/java/com/example/countries_details/Country.kt
@@ -0,0 +1,17 @@
+package com.example.countries_details
+
+import com.google.gson.annotations.SerializedName
+
+data class CountryDetailsResponse(private val details : List<Country>)
+
+data class Country {
+    @SerializedName("population") val population : Int,
+    @SerializedName("region") val region : String,
+    @SerializedName("subregion") val subregion : String,
+    @SerializedName("") val capital : String,
+
+    @SerializedName("language") val language : String,
+
+
+
+}
